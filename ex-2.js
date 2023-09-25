550d3f6cd92f5ce03efcc78c9145a56390c3bdf7
MSI\llxlust <llxlust.dev@gmail.com>
2023-09-25 19:42:15 +0700

    fixed-from-og

--- ex-2.js
+++ ex-2.js
@@ -1,13 +1,20 @@
 // Exercise #2: Promotion Conditions
 
 // Start coding here
-let lastMonthPaidMoreThan4000 = 4001 > 4000;
-let isWeekday = true;
-let hasBoughtProductFromITCategory = true;
-let hasAttendedDiscountEvent = false;
-let isPlatinum = false;
+let memberLevel = "Gold";
+let lastMonthPaid = 4001;
 
-let hasPromotion = (lastMonthPaidMoreThan4000 && isWeekday && hasBoughtProductFromITCategory && hasAttendedDiscountEvent) || isPlatinum
+let lastMonthPaidMoreThan4000 = lastMonthPaid > 4000;
+let isWeekday = false;
+let hasNotBoughtProductFromITCategory = true;
+let hasNotAttendedDiscountEvent = false;
+let isPlatinum = memberLevel == "Platinum";
 
+let hasPromotion =
+  (lastMonthPaidMoreThan4000 &&
+    !isWeekday &&
+    hasNotBoughtProductFromITCategory &&
+    hasNotAttendedDiscountEvent) ||
+  isPlatinum;
 
 console.log(hasPromotion);
--- ex-3.js
+++ ex-3.js
@@ -7,3 +7,4 @@ let numberB = 20;
 numberA = 10;
 
 console.log(`Sum: ${numberA + numberB}`);
+
