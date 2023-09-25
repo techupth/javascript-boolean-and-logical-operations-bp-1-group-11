21c68e1c16cac722a7914aa61d433bcf7355dbdd
MSI\llxlust <llxlust.dev@gmail.com>
2023-09-25 18:51:36 +0700

    fixed mistake

--- ex-2.js
+++ ex-2.js
@@ -1,12 +1,20 @@
 // Exercise #2: Promotion Conditions
 
 // Start coding here
-let lastMonthPaidMoreThan4000 = true;
-let isWeekday = true;
-let hasBoughtProductFromITCategory = false;
-let hasAttendedDiscountEvent = true;
-let isPlatinum = false;
+let memberLevel = "Gold";
+let lastMonthPaid = 4001;
 
-let hasPromotion = (lastMonthPaidMoreThan4000 && isWeekday && hasBoughtProductFromITCategory && hasAttendedDiscountEvent) || isPlatinum;
+let lastMonthPaidMoreThan4000 = lastMonthPaid > 4000;
+let isWeekday = false;
+let hasNotBoughtProductFromITCategory = true;
+let hasNotAttendedDiscountEvent = false;
+let isPlatinum = memberLevel == "Platinum";
+
+let hasPromotion =
+  (lastMonthPaidMoreThan4000 &&
+    !isWeekday &&
+    hasNotBoughtProductFromITCategory &&
+    hasNotAttendedDiscountEvent) ||
+  isPlatinum;
 
 console.log(hasPromotion);
